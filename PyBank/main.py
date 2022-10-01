4842fd8ad80ea5ece54fe03020a4369b29578447
bmilne64 <bronwynmilne64@gmail.com>
2022-10-01 13:15:59 -0400

    updates comments in files

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,9 +11,12 @@ with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     next(csvreader, None)
 
+    #print heading 
     print('Financial Analysis')
     print('----------------------------')
 
+    #define the values 
+    #set to 0 as a starting point 
     total_months = 0 
     profit_losses = 0
     change = 0
@@ -22,31 +25,52 @@ with open(csvpath, 'r') as csvfile:
     profit_decrease = 0 
     store_profit = 0
 
+    #start loop for csv 
     for row in csvreader:  
 
+        #count the total number of rows/months 
         total_months += 1
         
+        #for every row after the first, calacuate the change in value 
         if total_months > 1:
+
+            #the change is the current value - the stored prior value 
             change = (int(row[1]) - store_profit)
 
+        #average change = average change + change 
         average_change += change 
 
+        #define profit_losses
+        #profit losses = profit losses + the count of column 2
         profit_losses += int(row[1])
 
+        #define the stored prior value as the count of coulmn 2
         store_profit = int(row[1])
 
+        #if the change is greater than the stroed profit increase value 
         if change > profit_increase:
+
+            #the stored value becomes change 
             profit_increase = change 
+            #the date associated with the value change is saved 
             date_increase = row[0]
         
+       #if the change is less than the stroed profit increase value 
         if change < profit_decrease:
+
+            #the stored value becomes change 
             profit_decrease = change 
+            #the date associated with the value change is saved 
             date_decrease = row[0]
 
+    # average change = the number in average_change divided by the total times of change (85)
+    #no change for the first month (total_months - 1)
     average_change = (int(average_change)/int(total_months - 1))
+
+    #round the total change to 2 decimal points 
     average_change = round(average_change, 2)
 
-    
+    #print all statements 
     print('Total Months:' + str(total_months))
     print('Total: $' + str(profit_losses))
     print('Average Change: $' + str(average_change)) 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,54 +3,92 @@ import os
 import csv
 import sys 
 
-
+#open file for results 
 sys.stdout = open("results.txt", "a")
 
 #set path for file
 csvpath = os.path.join("..", "PyPoll", "Resources", "election_data.csv")
 
 # Open the CSV
-
 with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+    
+    #don't include the header 
     next(csvreader, None)
-
+    
+    #print the heading 
     print('Election Results')
     print('----------------------------')
 
+    #define the values 
     total_votes = 0 
     votes_list = []
     store_cand = ''
     win_votes = 0
     win_cand = ""
+
+    #start the loop for the csv
     for row in csvreader:   
-    
-        cands = (row[2])
-        votes_list.append(cands)
         
+        #define which column you want to look at 
+        cands = (row[2])
+
+        #append the list to include all the values 
+        votes_list.append(cands)
+    
+    #sort the list to put the names in order 
     votes_list.sort()
+
+    #print the length of the list to get the total number of votes 
     print('Total Votes:' + str(len(votes_list)))
     print('----------------------------')
     
+    #start the loop for the list 
     for cand in votes_list:
 
+        #if each name do not equal the prior stored name then 
         if cand != store_cand:
+
+            #count the number of times the name appears in the list to calculate amount of votes 
             votes = votes_list.count(cand)
+
+            #calculate the percetage of votes each candidate got 
             perc_votes = votes/len(votes_list)
+
+            #format the numbers as a percent 
             perc_votes = "{:.3%}".format(perc_votes)
+
+            #print the candidtae name, vote percentage and number of votes received 
             print(str(cand) +': ' + str(perc_votes) + ' (' + str(votes) + ')')
+            
+            # to find who got the highest number of votes 
+            #if the current votes are greater than the stored votes per candidate then 
             if votes > win_votes:
+
+                #the current votes become the stored winning votes 
                 win_votes = votes 
+                #the current candiate becomes the stored winning candidate 
                 win_cand = cand 
+            #reset the stored candidate to coutn the next candidate's votes 
             store_cand = cand 
 
 print('----------------------------')
 
+#print the candidate with the most votes after the loop 
 print('Winner: ' + str(win_cand))
 print('----------------------------')
 
+#close the results file 
 sys.stdout.close()
+
+#revert back to the terminal window 
 sys.stdout = sys.__stdout__
+
+#open the results file 
 with open('../PyPoll/results.txt') as file:
+
+    #read the results file 
     terminal = file.read()
+
+    #print the results to terminal 
     print(terminal)
